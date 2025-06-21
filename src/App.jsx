404dcc51112e5a9ff21862a175de8e22a318de40
theMaidkaitai <roman.khabarov.08@gmail.com>
2025-06-22 03:35:39 +0500

    нихуя не сделал2

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
 
-
-
 function App() {
-
-
   const tg = window.Telegram.WebApp
 
+  useEffect(() => {
+    tg.ready()
+  }, [])
 
   const onClose = () => {
     tg.close()
   }
-
 
   return (
     <div>
