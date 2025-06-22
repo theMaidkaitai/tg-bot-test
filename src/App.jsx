dff4d0c9bb484da9141cd80faa5dce805e3406a6
theMaidkaitai <roman.khabarov.08@gmail.com>
2025-06-23 03:56:04 +0500

    нихуя не сделал

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
       <Header/>
       <Routes>
         <Route index element= {<ProductList/>}/>
-        <Route path='form' element = {<Form/>}/>
+        <Route path={'form'} element = {<Form/>}/>
       </Routes>
     </div>
   )
