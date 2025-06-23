baf5bf9e4102e759c5b6539be9e38fed79ce6647
theMaidkaitai <roman.khabarov.08@gmail.com>
2025-06-23 07:48:26 +0500

    нихуя не сделал37

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -21,7 +21,31 @@ const getTotalPrice = (products) => {
 
 const ProductList = () => {
   const [addedItems, setAddedItems] = useState([])
-  const { tg } = useTelegram()
+  const { tg, queryId } = useTelegram()
+
+  const onSendData = useCallback(() => {
+    const data = {
+      products: addedItems,
+      totalPrice: getTotalPrice(addedItems),
+      queryId
+    }
+    fetch('http://localhost:8000', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data)
+    })
+  }, [])
+
+  useEffect(() => {
+    tg.onEvent('mainButtonClicked', onSendData)
+    return () => {
+      tg.offEvent('mainButtonClicked', onSendData)
+    }
+  }, [onSendData])
+
+
 
   const onAdd = (product) => {
     const alreadyAdded = addedItems.find(item => item.id === product.id)
