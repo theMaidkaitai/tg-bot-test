af7c5f7313faade6c8c2284676ecc26d49c7735a
theMaidkaitai <roman.khabarov.08@gmail.com>
2025-06-23 05:33:29 +0500

    нихуя не сделал36

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -17,9 +17,9 @@ const Form = () => {
     tg.sendData(JSON.stringify(data))
   }, [])
   useEffect(() => {
-    tg.WebApp.onEvent('mainButtonClicked', onSendData)
+    tg.onEvent('mainButtonClicked', onSendData)
     return () => {
-      tg.WebApp.offEvent('mainButtonClicked', onSendData)
+      tg.offEvent('mainButtonClicked', onSendData)
     }
   }, [])
 
